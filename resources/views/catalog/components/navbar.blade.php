74f2867f9250f606218160f790466280e7c49e8e
Henry Augusta <henryaugusta4@gmail.com>
2025-03-04 10:35:40 +0700

    add href for social media

--- resources/views/catalog/components/navbar.blade.php
+++ resources/views/catalog/components/navbar.blade.php
@@ -1,14 +1,25 @@
     <!-- Navbar -->
     <nav class="bg-jgc sticky top-0 z-50">
         <div class="container mx-auto flex items-center justify-between px-4 py-2">
-            <div class="flex items-center">
-                <img src="{{ asset('catalog/img/jgc-logo.png') }}" alt="JGC Logo" class="w-[115px] h-[115px]" />
-            </div>
+            <a href="/">
+                <div class="flex items-center">
+                    <img src="{{ asset('catalog/img/jgc-logo.png') }}" alt="JGC Logo" class="w-[115px] h-[115px]" />
+                </div>
+            </a>
             <div class="flex space-x-4">
-                <a href="#"><img src="{{ asset('catalog/img/fb.png') }}" alt="Facebook" class="w-5" /></a>
-                <a href="#"><img src="{{ asset('catalog/img/ig.png') }}" alt="Instagram" class="w-5" /></a>
-                <a href="#"><img src="{{ asset('catalog/img/yt.png') }}" alt="YouTube" class="w-5" /></a>
-                <a href="#"><img src="{{ asset('catalog/img/tiktok.png') }}" alt="TikTok" class="w-5" /></a>
+                <a href="https://www.facebook.com/JakartaGardenCityOfficialPage/?locale=id_ID" target="_blank">
+                    <img src="{{ asset('catalog/img/fb.png') }}" alt="Facebook" class="w-5" />
+                </a>
+                <a href="https://www.instagram.com/jakartagardencity/?hl=en" target="_blank">
+                    <img src="{{ asset('catalog/img/ig.png') }}" alt="Instagram" class="w-5" />
+                </a>
+                <a href="https://www.youtube.com/@jktgardencity/" target="_blank">
+                    <img src="{{ asset('catalog/img/yt.png') }}" alt="YouTube" class="w-5" />
+                </a>
+                <a href="https://www.tiktok.com/@official_jgc" target="_blank">
+                    <img src="{{ asset('catalog/img/tiktok.png') }}" alt="TikTok" class="w-5" />
+                </a>
             </div>
+
         </div>
     </nav>
